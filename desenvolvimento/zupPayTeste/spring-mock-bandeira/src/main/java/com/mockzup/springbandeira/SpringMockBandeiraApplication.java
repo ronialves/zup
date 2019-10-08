5f76b9d16dbb3804b6a6b4d9f59986b603d0d3d9
Roni Alves <ronialves25@gmail.com>
2019-10-08 19:39:17 -0300

    Inclusão do mock para a bandeira

---
+++ desenvolvimento/zupPayTeste/spring-mock-bandeira/src/main/java/com/mockzup/springbandeira/SpringMockBandeiraApplication.java
@@ -0,0 +1,13 @@
+package com.mockzup.springbandeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMockBandeiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMockBandeiraApplication.class, args);
+	}
+
+}
